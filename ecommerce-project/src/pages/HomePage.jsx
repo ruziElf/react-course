4c6e6772c213725bd4ae54b975b8b3e1050a84d1
User Name <user@gmail.com>
2025-12-04 12:19:34 +0100

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import { Header } from '../components/header.jsx';
 import { products } from '../../starting-code/data/products.js';
 import './HomePage.css';
+import { data } from 'react-router';
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response)=>{
+      return response.json()
+
+      }).then((data)=>{
+        console.log(data);
+    });
   return (
     <>
       <Header />
