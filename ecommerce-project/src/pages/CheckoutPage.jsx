9217cdee89e78bf45c7cf7e96fe231f58186553d
User Name <user@gmail.com>
2025-12-05 19:57:44 +0100

    Generate the Html for the orders page

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -137,25 +137,26 @@ export function CheckoutPage({ cart }) {
               <>
                 <div className="payment-summary-row">
                   <div>Items ({paymentSummary.totalItems}):</div>
-                  <div className="payment-summary-money">$42.75</div>
-                  {formatMoney(paymentSummary.productCostCents)}
+                  <div className="payment-summary-money">
+                    {formatMoney(paymentSummary.productCostCents)}</div>
+                  
                 </div>
 
                 <div className="payment-summary-row">
                   <div>Shipping &amp; handling:</div>
                   <div className="payment-summary-money">
-                    {formatMoney(paymentSummary.productCostCentsCost)}
+                   {formatMoney(paymentSummary.shippingCostCents)}
                   </div>
                 </div>
 
                 <div className="payment-summary-row subtotal-row">
                   <div>Total before tax:</div>
-                  <div className="payment-summary-money">{formatMoney(paymentSummary.shippingCostCents)}</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostBeforeTaxCents)}</div>
                 </div>
 
                 <div className="payment-summary-row">
                   <div>Estimated tax (10%):</div>
-                  <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostBeforeTaxCents)}</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.taxCents)}</div>
                 </div>
 
                 <div className="payment-summary-row total-row">
